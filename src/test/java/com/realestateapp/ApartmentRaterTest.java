c6501f172e7a6d032f7cc8c8af7380e341694c20
oscark22 <98506040+oscark22@users.noreply.github.com>
2023-03-03 10:44:20 -0800

    Initial commit

---
+++ src/test/java/com/realestateapp/ApartmentRaterTest.java
@@ -0,0 +1,93 @@
+package com.realestateapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApartmentRaterTest {
+
+    @ParameterizedTest
+    @ValueSource(doubles = {300, 400, 500, 600})
+    void should_ReturnCorrectRating_When_CorrectApartment(double area) {
+        // given
+        BigDecimal price = new BigDecimal(4000);
+        Apartment apartment = new Apartment(area, price);
+
+        // when
+        int apartmentRate = ApartmentRater.rateApartment(apartment);
+
+        // then
+        assertEquals(0, apartmentRate);
+    }
+
+    @Test
+    void should_ReturnErrorValue_When_IncorrectApartment() {
+        // given
+        double area = 0.0;
+        BigDecimal price = new BigDecimal(4000);
+        Apartment apartment = new Apartment(area, price);
+
+        // when
+        int apartmentRate = ApartmentRater.rateApartment(apartment);
+
+        // then
+        assertEquals(-1, apartmentRate);
+    }
+
+    @Test
+    void should_CalculateAverageRating_When_CorrectApartmentList() {
+        // given
+        ArrayList<Apartment> apartments = new ArrayList();
+        apartments.add(new Apartment(500, new BigDecimal(500)));
+        apartments.add(new Apartment(1000, new BigDecimal(500)));
+        apartments.add(new Apartment(500, new BigDecimal(1000)));
+
+        for (Apartment apartment : apartments) {
+            assertNotEquals(-1, ApartmentRater.rateApartment(apartment));
+        }
+    }
+
+    @Test
+    void should_ThrowExceptionInCalculateAverageRating_When_EmptyApartmentList() {
+        // given
+        ArrayList<Apartment> apartments = new ArrayList<>();
+
+        // when
+        Executable executable = () -> ApartmentRater.calculateAverageRating(apartments);
+
+        // then
+        assertThrowsExactly(RuntimeException.class, executable);
+    }
+
+    @Test
+    void should_GenerateCorrectApartment_When_DefaultMinAreaMinPrice() {
+        // given
+        RandomApartmentGenerator generator = new RandomApartmentGenerator();
+        Apartment apartment = generator.generate();
+
+        // when
+        int apartmentRate = ApartmentRater.rateApartment(apartment);
+
+        // then
+        assertNotEquals(-1, apartmentRate);
+    }
+
+    @Test
+    void should_GenerateCorrectApartment_When_CustomMinAreaMinPrice() {
+        // given
+        RandomApartmentGenerator generator = new RandomApartmentGenerator(1, new BigDecimal(0));
+        Apartment apartment = generator.generate();
+
+        // when
+        int apartmentRate = ApartmentRater.rateApartment(apartment);
+
+        // then
+        assertNotEquals(-1, apartmentRate);
+    }
+}
